933f9a13be7a7419de3abd0f743075f51bafc6d1
Angel Nikolov <angel.nikolov@mentormate.com>
2018-04-05 01:33:53 +0300

    Updated to Chrome HEadless

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
   config.set({
     frameworks: ["jasmine", "karma-typescript"],
-    browsers: ["Chrome"],
+    browsers: ["ChromeHeadless"],
     reporters: ["progress", "karma-typescript"],
     files: ["./cacheable.decorator.ts", "./cacheable.decorator.spec.ts"],
     preprocessors: {
